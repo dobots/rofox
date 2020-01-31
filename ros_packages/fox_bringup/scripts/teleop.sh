47bd8c08df8de734d80a083cf5c8e67f8656b0e5
thomas <thomas@dobots.nl>
2020-01-31 12:47:18 +0000

    set tf transform lidar, added launch file teleop_fox package

--- ros_packages/fox_bringup/scripts/teleop.sh
+++ ros_packages/fox_bringup/scripts/teleop.sh
@@ -7,6 +7,7 @@ source /opt/ros/kinetic/setup.bash
 source /home/husarion/ros_workspace/devel/setup.sh
 
 sudo sixad -s &
-roslaunch fox_bringup teleop.launch
+#roslaunch fox_bringup fox.launch &
+roslaunch teleop_twist_joy_fox teleop_fox.launch &
 
 wait
