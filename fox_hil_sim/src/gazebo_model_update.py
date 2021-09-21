947b75f1c50befd815c9239ca72d14982f2c020b
Reka-Berci-Hajnovics <reka.hajnovics@gmail.com>
2021-09-21 16:20:03 +0200

    startup instructions

--- fox_hil_sim/src/gazebo_model_update.py
+++ fox_hil_sim/src/gazebo_model_update.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
 import rospy
-# from geometry_msgs.msg import PoseStamped
-from nav_msgs.msg import Odometry
+from geometry_msgs.msg import PoseStamped
+#from nav_msgs.msg import Odometry
 from gazebo_msgs.msg import ModelState
 
 rospy.init_node('gazebo_model_update')
@@ -16,14 +16,15 @@ def odomCallback(msg):
   global pose_gazebo
 
   # fill gazebo position with the pose in the odom message
-  pose_gazebo.pose = msg.pose.pose
-
+  #pose_gazebo.pose = msg.pose.pose
+  pose_gazebo.pose = msg.pose
+  
   # publish the gazebo pose
   model_pub.publish(pose_gazebo)
 
 # initialize subscriber and publisher
-odom_sub = rospy.Subscriber('odom', Odometry, odomCallback)
-# odom_sub = rospy.Subscriber('pose', PoseStamped, odomCallback)
+#odom_sub = rospy.Subscriber('odom', Odometry, odomCallback)
+odom_sub = rospy.Subscriber('pose', PoseStamped, odomCallback)
 model_pub = rospy.Publisher('gazebo/set_model_state', ModelState, queue_size = 1)
 
 rate = rospy.Rate(20) # 20 Hz
